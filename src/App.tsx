a520ee92a30a2b462636c8679f4816e4adf3c7f3
heimdallrj <mailtokasun@gmail.com>
2025-04-16 01:53:30 +0200

    poc: serve markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [content, setContent] = useState("")
+
+  async function load() {
+    const url = "/__blog__/2025-01-01/1735723445-hello.md";
+    const response = await fetch(url)
+    const markdown = await response.text()
+    setContent(markdown)
+  }
+
+  useEffect(() => {
+    load();
+  }, [])
+
   return (
     <>
       <div>
@@ -13,7 +29,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>hydra-gh</h1>
+
+      <ReactMarkdown>{content}</ReactMarkdown>
     </>
   )
 }
